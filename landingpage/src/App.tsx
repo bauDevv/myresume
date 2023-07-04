798e2eaa30f73a06df123890b8526174f8f40dbf
Alfredo Bautista <abautista200214@gmail.com>
2023-07-04 17:59:47 -0600

    Create webpage

---
+++ landingpage/src/App.tsx
@@ -0,0 +1,62 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div className='container'>
+        <div className='navbar bg-base'>
+          <div className='flex-1'>
+            <a href="" className='btn btn-ghost normal-case text-xl'>Alfredo Bautista</a>
+          </div>
+          <div className='flex-none'>
+            <ul className='menu menu-horizontal px-1'>
+              <li>
+                <details>
+                  <summary>About me</summary>
+                  <ul className='p-2 bg-base-100'>
+                    <li>  
+                      <a href="#training">My training</a>
+                    </li>
+                    <li>
+                      <a href="#certifications">Certifications</a>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <a href="#tech">Technologies and Skills</a>
+              </li>
+              <li>
+                <a href="#portfolio">PortFolio</a>
+              </li>
+            </ul>
+            
+          </div>
+         
+        </div>
+      <div>
+        <section>
+
+        </section>
+        <section>
+          <div className='container'>
+
+          </div>
+        </section>
+        <section>
+          
+        </section>
+        <section>
+          
+        </section>
+        <section>
+          
+        </section>
+      </div>
+      </div>
+    </div>
+   
+  )
+}
+
+export default App
